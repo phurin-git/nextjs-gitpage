6689e9ca05f4e44e348a371ee95dc945c9f8689c
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-23 16:18:45 +0700

    Update commit
    Fix: getReponame() run so many time and make basepath of asset abnormal then change to just static string

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,9 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const getRepoName = () => {
-    try {
-        const repoUrl = execSync('git config --get remote.origin.url').toString().trim();
-        const repoName = repoUrl.split('/').pop().replace('.git', '');
-        return repoName;
-    } catch (error) {
-        console.error('Error getting repository name:', error);
-        return '';
-    }
-};
-
-const repoName = getRepoName();
-
 const nextConfig = isProd ? {
     env: {
-        BASE_URL: `/${repoName}`,
+        BASE_URL: '/static-export-nextjs',
     },
     basePath: process.env.BASE_URL,
     assetPrefix: process.env.BASE_URL,
