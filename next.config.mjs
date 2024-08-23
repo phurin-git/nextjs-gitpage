06682de1b62f2f539fd535b9ebff1c883c0f6ecf
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-23 15:40:03 +0700

    Update commit
    - Renew config "next.config.mjs"
    - Add "postbuild" in "package.json" for automatic echo ".nojekyll"
    - Add basepath at next image src component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/static-export-nextjs',
-    assetPrefix: '/static-export-nextjs',
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const getRepoName = () => {
+    try {
+        const repoUrl = execSync('git config --get remote.origin.url').toString().trim();
+        const repoName = repoUrl.split('/').pop().replace('.git', '');
+        return repoName;
+    } catch (error) {
+        console.error('Error getting repository name:', error);
+        return '';
+    }
+};
+
+const repoName = getRepoName();
+
+const nextConfig = isProd ? {
+    env: {
+        BASE_URL: `/${repoName}`,
+    },
+    basePath: process.env.BASE_URL,
+    assetPrefix: process.env.BASE_URL,
     output: 'export',
     distDir: 'docs',
-};
+} : {};
 
 export default nextConfig;
