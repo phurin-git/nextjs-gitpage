6d664a89b1b7b58740e305b1df15acc106ecc502
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-23 11:42:25 +0700

    Update page.tsx
    Add: ts-ignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
-import vercelLogo from "/public/vercel.svg"
-import nextLogo from "/public/next.svg"
+// @ts-ignore
+import vercelLogo from "/public/vercel.svg"; 
+// @ts-ignore
+import nextLogo from "/public/next.svg";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
