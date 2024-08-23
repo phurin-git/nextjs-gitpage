dd2a63679fb5dd92f7602f0d79df6b10bb0db16b
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-23 16:36:14 +0700

    Update commit
    Fix: the true bug I can't use env variable with basepath and assetprefix (but I still can get reponame without problem)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 /** @type {import('next').NextConfig} */
+import { execSync } from 'child_process';
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const getRepoName = () => {
+    try {
+        const repoUrl = execSync('git config --get remote.origin.url').toString().trim();
+        const repoName = repoUrl.split('/').pop().replace('.git', '');
+        return repoName;
+    } catch (error) {
+        console.error('Error getting repository name:', error);
+        return '';
+    }
+};
+
+const repoName = getRepoName();
 const nextConfig = isProd ? {
-    basePath: '/static-export-nextjs',
-    assetPrefix: '/static-export-nextjs',
+    env: {
+        BASE_URL: `/${repoName}`,
+    },
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}`,
     output: 'export',
     distDir: 'docs',
 } : {};
